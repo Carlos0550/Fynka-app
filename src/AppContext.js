078908b3498e636cf3b4cfe1aa5990e68beb1b6a
carlos0550 <carlospelinski03@gmail.com>
2024-11-23 11:17:09 -0300

    Agrega funcion para verificar la autenticacion del usuario

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,7 +1,8 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useRef, useState } from "react";
 import { baseUrl } from "./api";
 import { message, notification } from "antd";
 import { apiResponses } from "./apiTexts";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext()
 
@@ -12,6 +13,8 @@ export const useAppContext = () => {
 }
 
 export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate()
+
     const registerBusiness = async (registerData) => {
         try {
             const response = await fetch(`${baseUrl.api}/create-administrator`, {
@@ -41,6 +44,7 @@ export const AppContextProvider = ({ children }) => {
         }
     }
 
+    const [loginUserData, setLoginUserData] = useState(null)
     const login = async (loginData) => {
         try {
             const response = await fetch(`${baseUrl.api}/login-user`, {
@@ -49,27 +53,34 @@ export const AppContextProvider = ({ children }) => {
             })
 
             const data = await response.json()
-            if(response.status === 404){
+            if (response.status === 404) {
                 notification.error({
                     message: "Usuario o contraseña inválido",
                     duration: 3,
                     pauseOnHover: false,
                     showProgress: true
                 });
-                return;
+                return false;
             };
-            if(response.status === 401){
+            if (response.status === 401) {
                 notification.error({
                     message: data.msg,
                     duration: 3,
                     pauseOnHover: false,
                     showProgress: true
                 });
-                return;
+                return false;
             };
             if (!response.ok) throw new Error(data.msg)
             message.success(`${data.msg}`)
-        console.log(data)
+            setLoginUserData(data.usrData)
+            const dataUser = JSON.parse(localStorage.getItem("userdata"))
+            if (!dataUser) {
+                localStorage.setItem("userdata", JSON.stringify(data.usrData));
+            } else {
+                localStorage.removeItem("userdata");
+                localStorage.setItem("userdata", JSON.stringify(data.usrData));
+            }
             return true
         } catch (error) {
             console.log(error)
@@ -84,10 +95,53 @@ export const AppContextProvider = ({ children }) => {
         }
     }
 
+    const alreadyVerified = useRef(false)
+    const verifyAuthUser = async () => {
+        if (!alreadyVerified.current) {
+            const userData = localStorage.getItem("userdata");
+            alreadyVerified.current = true
+            try {
+                if (userData) {
+                    const parseData = JSON.parse(userData);
+                    if (!parseData.nombre_usuario || !parseData.email) {
+                        throw new Error("Datos incompletos en localStorage. Por favor, inicie sesión nuevamente.");
+                    }
+
+                    const response = await fetch(
+                        `${baseUrl.api}/verifyAuthUser?username=${encodeURIComponent(parseData.nombre_usuario)}&email=${encodeURIComponent(parseData.email)}`
+                    );
+
+                    if (!response.ok) {
+                        const errorData = await response.json();
+                        throw new Error(errorData?.msg || "Error desconocido al verificar autenticación.");
+                    }
+                    if (!loginUserData) setLoginUserData(userData)
+                    message.success("Bienvenido nuevamente.");
+                    return true
+                } else {
+                    throw new Error("Datos incompletos en localStorage. Por favor, inicie sesión nuevamente.");
+                }
+            } catch (error) {
+                console.error(error);
+                navigate("/")
+                notification.error({
+                    message: error.status === 401 ? "Sesión expirada" : "Error al intentar iniciar sesión",
+                    description: error.message || apiResponses.error,
+                    duration: 5,
+                    showProgress: true,
+                    pauseOnHover: false,
+                });
+                return false
+            }
+        }
+    };
+
+
     return (
         <AppContext.Provider
             value={{
-                registerBusiness,login
+                registerBusiness, login, loginUserData,
+                verifyAuthUser
             }}
         >
             {children}
