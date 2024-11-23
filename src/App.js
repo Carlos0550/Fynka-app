57a24da8e7706cdb50ff437330d7b0c59beb50ba
carlos0550 <carlospelinski03@gmail.com>
2024-11-23 11:16:50 -0300

    Agrega ruta para el dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
-import AuthWrapper from './pages/AuthWrapper'
+import AuthWrapper from './pages/Auth/AuthWrapper'
+import Dashboard from './pages/dashboard/Dashboard'
 function App() {
   return (
     <Routes>
       <Route path='/' element={<AuthWrapper/>}/>
+      <Route path='/dashboard/*' element={<Dashboard/>}/>
     </Routes>
   )
 }
