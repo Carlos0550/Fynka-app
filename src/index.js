63b9aa6dd3c014e8438fac30c9d2e678cbb5a78a
carlos0550 <carlospelinski03@gmail.com>
2024-11-25 14:03:37 -0300

    Agrega proveedor de configuracion der antd y su locale en español

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContextProvider } from './AppContext';
+import { ConfigProvider } from 'antd';
+import esES from "antd/locale/es_ES"
 
 ReactDOM.render(
   <BrowserRouter>
     <AppContextProvider>
-      <App />
+      <ConfigProvider locale={esES}>
+        <App />
+      </ConfigProvider>
     </AppContextProvider>
   </BrowserRouter>,
   document.getElementById('root')
