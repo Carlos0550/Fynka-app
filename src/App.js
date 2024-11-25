8ad6733a6e9eec7f95e80beca46133afc9c1e273
carlos0550 <carlospelinski03@gmail.com>
2024-11-25 14:03:19 -0300

    Agrega rutas para clientes y sucursales

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import AuthWrapper from './pages/Auth/AuthWrapper'
 import Dashboard from './pages/dashboard/Dashboard'
+import ClientsManager from './pages/Clientes/ClientsManager'
+import SucursalesManager from './pages/Sucursales/SucursalesManager'
 function App() {
   return (
     <Routes>
       <Route path='/' element={<AuthWrapper/>}/>
-      <Route path='/dashboard/*' element={<Dashboard/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/clientes/*' element={<ClientsManager/>}/>
+      <Route path='/sucursales' element={<SucursalesManager/>}/>
     </Routes>
   )
 }
