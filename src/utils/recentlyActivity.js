197934abf955fa73875bae58e340119e416a0ec9
carlos0550 <carlospelinski03@gmail.com>
2024-11-25 14:05:46 -0300

    Array de pruebas de actividad reciente

---
+++ src/utils/recentlyActivity.js
@@ -0,0 +1,32 @@
+export const recentlyActivity = [
+    {
+        id: 1,
+        client_name: "Carlos Pelinski",
+        type: "Entrega",
+        mount: "45000"
+    },
+    {
+        id: 2,
+        client_name: "Carlos Pelinski",
+        type: "Entrega",
+        mount: "45000"
+    },
+    {
+        id: 3,
+        client_name: "Carlos Pelinski",
+        type: "Entrega",
+        mount: "45000"
+    },
+    {
+        id: 4,
+        client_name: "Amelia Pelinski",
+        type: "Deuda",
+        productsCount: 5
+    },
+    {
+        id: 5,
+        client_name: "Anastacia Cabrera",
+        type: "Deuda",
+        productsCount: 3
+    },
+]
