83c946f04797e67e84283b8e961c15e8bcc2a086
carlos0550 <carlospelinski03@gmail.com>
2024-11-27 15:54:19 -0300

    Muestra sucursales

--- src/pages/Sucursales/Modales/ShowBranchesForm.jsx
+++ src/pages/Sucursales/Modales/ShowBranchesForm.jsx
@@ -1,15 +1,29 @@
 import { Modal } from 'antd'
 import React from 'react'
-import AddBranches from '../Agregar_sucursales/AddBranches'
+import AddBranches from '../Formularios/Añadir_sucursales/AddBranches'
+import EditBranches from '../Formularios/Editar_sucursales/EditBranches'
 
-function ShowBranchesForm({closeModal}) {
+function ShowBranchesForm({closeModal, actionType, branchId}) {
+
+  function switchForms() {
+    switch (actionType) {
+      case 1:
+        
+        return <AddBranches closeModal={closeModal}/>
+      
+    case 2:
+        return <EditBranches branchId={branchId} closeModal={closeModal}/>
+      default:
+        return "Ups... Tenemos problemas para mostrar esta sección, intente nuevamente en unos segundos."
+    }
+  }
   return (
     <Modal
         open={true}
         onCancel={closeModal}
         footer={[]}
     >
-        <AddBranches/>
+        {switchForms()}
     </Modal>
   )
 }
