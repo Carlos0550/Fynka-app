46aa73eecc70887dc4dc21390f4b46593d104293
carlos0550 <carlospelinski03@gmail.com>
2024-11-29 14:44:14 -0300

    Muestra titulos personalizados, y agrega entrada para mostrar la alerta de eliminado de cliente, y edicion

--- src/pages/Clientes/Modales/ClientFormModal.jsx
+++ src/pages/Clientes/Modales/ClientFormModal.jsx
@@ -1,17 +1,32 @@
 import { Modal } from 'antd'
 import React from 'react'
 import CreateClient from '../Formularios/CreateClient';
+import EditClient from '../Formularios/EditClient';
+import DeleteClientAlert from '../Alertas/DeleteClientAlert';
 
-function ClientFormModal({ closeModal, actionType }) {
+function ClientFormModal({ closeModal, actionType, clientId }) {
+
+    const titleKeys = {
+        1: "Crear un cliente",
+        2: "Actualizar un cliente",
+        3: "¿Seguro de querer eliminar este cliente?"
+    }
 
     function renderFormByOption() {
         switch (actionType) {
-            case "1":
+            case 1:
                 
                 return(
                     <CreateClient closeModal={closeModal}/>
                 )
-        
+            case 2:
+                return (
+                    <EditClient closeModal={closeModal} clientID={clientId}/>
+                )
+            case 3:
+                return(
+                    <DeleteClientAlert closeModal={closeModal} clientID={clientId}/>
+                )
             default:
                 return "Oops... Tenemos problemas para mostrar esta sección."
         }
@@ -21,6 +36,7 @@ function ClientFormModal({ closeModal, actionType }) {
             open={true}
             onCancel={closeModal}
             footer={[]}
+            title={titleKeys[actionType] || "Acción desconocida"}
         >
             {renderFormByOption()}
         </Modal>
