0b41b77b10c8b0cbeec93de118134d67b5bc0764
carlos0550 <carlospelinski03@gmail.com>
2024-12-03 16:24:47 -0300

    Renderiza de forma dinamica las tablas

---
+++ src/pages/Clientes/Cuentas_corrientes/RenderTable.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Debts from './Tables/Debts'
+import Delivers from './Tables/Delivers'
+import { Empty } from 'antd'
+function RenderTable({tableId, state}) {
+    console.log(state)
+    function RenderTable() {
+        switch (parseInt(tableId)) {
+            case 1:
+                
+                return(
+                    <Debts/>
+                )
+            case 2: 
+                return(
+                    <Delivers/>
+                )
+            default:
+                <Empty/>
+        }
+    }
+  return (
+    <React.Fragment>
+        {RenderTable()}
+    </React.Fragment>
+  )
+}
+
+export default RenderTable
