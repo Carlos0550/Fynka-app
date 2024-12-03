5521c1ec019a66d42d33fe55ca90ba9aadfadabf
carlos0550 <carlospelinski03@gmail.com>
2024-12-03 16:23:59 -0300

    Ejectos visuales

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Layout.css";
 import ClientsIcon from "../../assets/ClientsIcon"
 import Home from "../../assets/Home"
@@ -17,11 +17,25 @@ function Layout({ children }) {
         { path: "/usuarios", label: "Usuarios", icon: <UsersIcon/>},
         { path: "/configuracion", label: "Ajustes", icon: <SettingsIcon/> },
     ];
+    const [sidebarActive, setSidebarActive] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
 
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    },[])
+    useEffect(()=>{
+        if(width >= 768){
+            setSidebarActive(true)
+        }else{
+            setSidebarActive(false)
+        }
+    },[width])
     return (
         <React.Fragment>
             <div id="layout__wrapper">
-                <input type="checkbox" id="sidebar-toggle-checkbox" />
+                <input type="checkbox" id="sidebar-toggle-checkbox" checked={sidebarActive} onClick={()=>setSidebarActive(!sidebarActive)}/>
                 <div class="sidebar">
                     <label for="sidebar-toggle-checkbox" class="sidebar-toggle">☰</label>
                     {routePaths.map((routeObj) => (
