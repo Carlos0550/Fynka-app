28a0e6051e905337e1d8d0cec45eeeee139cc5e5
carlos0550 <carlospelinski03@gmail.com>
2024-12-06 19:00:37 -0300

    Agrega ajustes de logica y la tabla de visualizacion de entregas

--- src/pages/Clientes/Cuentas_corrientes/RenderTable.jsx
+++ src/pages/Clientes/Cuentas_corrientes/RenderTable.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Debts from './Tables/Debts'
 import Delivers from './Tables/Delivers'
-import { Empty } from 'antd'
+import { Button, Empty } from 'antd'
 function RenderTable({tableId}) {
+    const [openDebtModal, setOpenDebtmodal] = useState(false)
+    const [openDeliverModal, setOpenDeliverModal] = useState(false)
     function RenderTable() {
         switch (parseInt(tableId)) {
             case 1:
                 
                 return(
-                    <Debts />
+                    <React.Fragment>
+                        <Button onClick={()=> setOpenDebtmodal(true)} style={{marginTop: "1rem"}}>
+                            Agregar deuda
+                        </Button>
+                        <Debts openModal={openDebtModal} setOpenmodal={setOpenDebtmodal}/>
+                    </React.Fragment>
                 )
             case 2: 
                 return(
-                    <Delivers />
+                    <React.Fragment>
+                        <Button onClick={()=> setOpenDeliverModal(true)} style={{marginTop: "1rem"}}>
+                            Agregar entrega
+                        </Button>
+                        <Delivers openModal={openDeliverModal} setOpenmodal={setOpenDeliverModal}/>
+                    </React.Fragment>
                 )
             default:
                 <Empty/>
