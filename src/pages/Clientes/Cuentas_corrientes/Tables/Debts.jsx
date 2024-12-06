7deaa4b1f3f7252a92f7b100639de53a337f906a
carlos0550 <carlospelinski03@gmail.com>
2024-12-06 11:43:29 -0300

    Agrega visualizacion de las deudas

--- src/pages/Clientes/Cuentas_corrientes/Tables/Debts.jsx
+++ src/pages/Clientes/Cuentas_corrientes/Tables/Debts.jsx
@@ -1,13 +1,51 @@
 import { Button, Table } from 'antd'
 import React, { useState } from 'react'
 import ShowAccountsForms from '../Modales/ShowAccountsForms'
-
+import { useAppContext } from '../../../../AppContext'
+import { processData } from './processData'
+import dayjs from 'dayjs'
 function Debts() {
   const [openModal, setOpenmodal] = useState(false)
+  const { gettingAccount, clientDebts } = useAppContext()
   const handleOpenModal = () => {
     setOpenmodal(true)
   }
-    
+  console.log(clientDebts)
+  const tableColumns =  [
+    {
+      title: "Fecha de compra",
+      render: (_,record) => (
+        <React.Fragment>
+          <strong>{dayjs(record.created_at).format("DD/MM/YYYY")}</strong>
+          <p>Con vencimiento el <strong>{dayjs(record.vencimiento).format("DD/MM/YYYY")}</strong></p>
+        </React.Fragment>
+      )
+    },
+    {
+      title: "Detalles",
+      render: (_,record) => {
+        const products = processData(record.detalle_productos)
+        return(
+          <React.Fragment>
+            <ul>
+              {products.map(product => (
+                <li>{product.quantity} {product.name} {parseFloat(product.price).toLocaleString("es-AR",{style:"currency", "currency": "ARS"})}</li>
+              ))}
+            </ul>
+          </React.Fragment>
+        )
+      }
+    },
+    {
+      title: "Detalles Adicionales",
+    },
+    {
+      title: "Monto total",
+      render: (_,record) => (
+        <strong>{parseFloat(record.monto_total).toLocaleString("es-AR",{style:"currency", "currency": "ARS"})}</strong>
+      )
+    }
+  ]
   return (
     <React.Fragment>
       <Table
@@ -22,6 +60,9 @@ function Debts() {
           </>
         )
       }}
+      loading={gettingAccount}
+      columns={tableColumns}
+      dataSource={clientDebts}
     >
     </Table>
     {openModal && 
