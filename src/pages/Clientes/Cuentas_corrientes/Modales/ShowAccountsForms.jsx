7f9ec86a78d29c41cb0fb8fe7148ca0a66af0e85
carlos0550 <carlospelinski03@gmail.com>
2024-12-06 19:00:56 -0300

    Agrega titulos y la tabla de entregas

--- src/pages/Clientes/Cuentas_corrientes/Modales/ShowAccountsForms.jsx
+++ src/pages/Clientes/Cuentas_corrientes/Modales/ShowAccountsForms.jsx
@@ -1,16 +1,20 @@
 import { Modal } from 'antd'
 import React from 'react'
 import AddDebt from '../Formularios/AddDebt'
+import AddDeliver from '../Formularios/AddDeliver'
 
 function ShowAccountsForms({closeModal, actionType}) {
   const titleKeys = {
-    1: "Agregar una deuda"
+    1: "Agregar una deuda",
+    2: "Agregar una entrega"
   }
 
   const handleSwitchForm = () =>{
     switch (parseInt(actionType)) {
       case 1:
-        return(<AddDebt closeModal={closeModal}/>)  
+        return(<AddDebt closeModal={closeModal}/>)
+      case 2:
+        return (<AddDeliver closeModal={closeModal} />)  
       default:
         break;
     }
