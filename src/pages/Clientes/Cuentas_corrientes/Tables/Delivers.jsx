a0d56279eba68cb8ee38b7de6286a47e14873151
carlos0550 <carlospelinski03@gmail.com>
2024-12-07 00:13:35 -0300

    Visualiza las entregas

--- src/pages/Clientes/Cuentas_corrientes/Tables/Delivers.jsx
+++ src/pages/Clientes/Cuentas_corrientes/Tables/Delivers.jsx
@@ -1,8 +1,35 @@
-import { Button, Table } from 'antd'
+import { Button, Space, Table } from 'antd'
 import React from 'react'
 import ShowAccountsForms from '../Modales/ShowAccountsForms'
+import { useAppContext } from '../../../../AppContext'
+import dayjs from 'dayjs'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
 
 function Delivers({ openModal, setOpenmodal }) {
+  const { clientMoneyDelivers, gettingAccount } = useAppContext()
+
+  const tableColumns = [
+    {
+      title: "Fecha de entrega",
+      render:(_,record)=>(
+        <p>{dayjs(record.fecha).format("DD/MM/YYYY")}</p>
+      )
+    },
+    {
+      title: "Monto de entrega",
+      render:(_,record)=>(
+        <p>{parseFloat(record.monto).toLocaleString("es-AR",{style:"currency", "currency": "ARS"})}</p>
+      )
+    },
+    {
+      render:(_,record) => (
+        <Space direction='vertical'>
+          <Button type='primary' icon={<EditOutlined />}/>
+          <Button type='primary' danger icon={<DeleteOutlined />}/>
+        </Space>
+      )
+    }
+  ]
   return (
     <React.Fragment>
       <Table
@@ -14,6 +41,10 @@ function Delivers({ openModal, setOpenmodal }) {
             </React.Fragment>
           )
         }}
+        loading={gettingAccount}
+        style={{marginTop: ".5rem"}}
+        columns={tableColumns}
+        dataSource={clientMoneyDelivers}
       />
 
       {openModal && <ShowAccountsForms closeModal={()=> setOpenmodal(false)} actionType={2}/>}
