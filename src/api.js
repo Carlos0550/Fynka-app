53b24d0c1950d3ab0e547509817c77788cbe7f8c
carlos0550 <carlospelinski03@gmail.com>
2024-12-07 00:13:53 -0300

    Implementado guardado de entregas

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const baseUrl={
-    api: "https://fynka-server-production.up.railway.app",
+    api: "http://localhost:4000",
     frontApi: "http://localhost:3000"
 }
 
-// 
+// https://fynka-server-production.up.railway.app
